ccbde97dc514ab44cf687f7294a295ff392220b7
Constanza <constanza.vp.o@gmail.com>
2024-02-14 13:05:55 -0300

    Agregando elementos con .push .unshift y .splice

---
+++ app.js
@@ -0,0 +1,11 @@
+let superHeroes= [
+    "Ironman",
+    "Superman",
+    "Hawkeye"
+]
+
+superHeroes.push("Antman")
+superHeroes.unshift("Dr. Strange")
+superHeroes.splice(2 , 0, "spiderman")
+
+console.log(superHeroes)
