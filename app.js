e20d8dfb6f69dec1df4bc0ae988e7934e00968f2
Constanza <constanza.vp.o@gmail.com>
2024-02-14 13:24:15 -0300

    Agregando y eliminando elementos con .pop .unshift y .splice

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// Agregando elementos al arreglo
+
 let superHeroes= [
     "Ironman",
     "Superman",
@@ -9,3 +11,14 @@ superHeroes.unshift("Dr. Strange")
 superHeroes.splice(2 , 0, "spiderman")
 
 console.log(superHeroes)
+
+// Agregando y eliminando elementos del arreglo
+
+const usuarios = ["Erick", "Cristian", "Max", "Claudia"];
+
+usuarios.pop()
+usuarios.unshift("María José")
+usuarios.splice(2 , 1)
+
+
+console.log(usuarios)
