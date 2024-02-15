799736f6acf0a9f5dc9b455ad00c09488f47b54d
Constanza <constanza.vp.o@gmail.com>
2024-02-15 12:34:32 -0300

    Actividad 3

--- app.js
+++ app.js
@@ -22,3 +22,42 @@ usuarios.splice(2 , 1)
 
 
 console.log(usuarios)
+
+// Buscando el indice de un elemento
+
+const apellidos = ["Aniston", "Cox", "Buffay", "Perry", "LeBlanc", "Schwimmer"]
+
+const indiceDePerry = apellidos.findIndex( apellido => apellido === "Perry" )
+console.log(indiceDePerry); 
+
+//////////////7
+let actores = [
+    { id: 431, nombre: "Jhonny Depp" },
+    { id: 124, nombre: "Brad Pitt" },
+    { id: 541, nombre: "Leonardo DiCaprio" },
+    { id: 664, nombre: "Morgan Freeman" }
+];
+
+const indiceEliminarActor = actores.findIndex(actor => actor.id === 541)
+
+actores.splice(indiceEliminarActor,1)
+
+console.log(actores)
+
+
+// Actividad 3
+
+const peliculas = [
+    {id: 1, nombre: "Thor"},
+    {id: 2, nombre: "Ant-Man"},
+    {id: 3, nombre: "Terminator"},
+    {id: 4, nombre: "Ip Man"},
+    {id: 5, nombre: "Rocky"},
+    ]
+
+    const encontrarPelicula = peliculas.findIndex(pelicula => pelicula.nombre === "Terminator")
+
+    peliculas.splice(encontrarPelicula,1)
+
+    console.log(encontrarPelicula)
+    console.log(peliculas)
