2a03f43f380d9593a8b76b7e6b5fbf48a8ff8596
Constanza <constanza.vp.o@gmail.com>
2024-02-15 16:48:49 -0300

    actividad 8

--- arreglosYDom.js
+++ arreglosYDom.js
@@ -3,18 +3,21 @@ const listaDeInvitados = document.getElementById("invitados")
 const imput = document.getElementById("nuevoInvitado")
 
 const invitados = []
-btnAgregar.addEventListener("click", () => {
-    const nuevoInvitado = imput.value
-    invitados.push({id: Date.now(), nombre: nuevoInvitado})
-    imput.value= ""
 
-
+function renderInvitados(){
     let template = ""
     for(let invitado of invitados){
     template += `<li> ${invitado.nombre} <button onclick="borrar(${invitado.id})">Eliminar</button> </li>`;
     }
-
     listaDeInvitados.innerHTML=template;
+}
+
+btnAgregar.addEventListener("click", () => {
+    const nuevoInvitado = imput.value
+    invitados.push({id: Date.now(), nombre: nuevoInvitado})
+    imput.value= ""
+    renderInvitados()
+
     console.log(invitados)
 })
 
@@ -22,13 +25,7 @@ btnAgregar.addEventListener("click", () => {
 function borrar (id){
     const index = invitados.findIndex((ele) => ele.id == id)
     invitados.splice(index,1)
-
-    let template = ""
-    for(let invitado of invitados){
-    template += `<li> ${invitado.nombre} <button onclick="borrar(${invitado.id})">Eliminar</button> </li>`;
-    }
-
-    listaDeInvitados.innerHTML=template;
+    renderInvitados()
 }
 
 
